1d4b72c846727c7bba5ec69872541b810b07fd7b
Monika Rychlicka <monika.k.rychlicka@gmail.com>
2019-12-07 11:02:44 +0000

    Added main file

---
+++ ArrayExample.java
@@ -0,0 +1,15 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        String[] week = new String[7];
+        //System.out.println(week[0]);
+        week[0] = "Poniedzialek";
+        week[1] = "Wtorek";
+        week[2] = "Sroda";
+        week[3] = "Czwartek";
+        week[4] = "Piatek";
+        week[5] = "Sobota";
+        week[6] = "Niedziela";
+        System.out.println(week[0]);
+
+    }
+}
